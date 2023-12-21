590f406ec36265461a67724ddf955446c8e602fe
Ahmed <ahmedzulkarnaev9500@gmail.com>
2023-12-21 12:27:28 +0300

    finally api

--- yatube_api/api/urls.py
+++ yatube_api/api/urls.py
@@ -3,14 +3,14 @@ from rest_framework.authtoken import views
 from rest_framework.routers import DefaultRouter
 
 from .views import CommentViewSet, GroupViewSet, PostViewSet
-
+from .variables import API_VERSION
 
 router = DefaultRouter()
-router.register(r'posts', PostViewSet)
+router.register('posts', PostViewSet)
 router.register(r'posts/(?P<post_id>\d+)/comments', CommentViewSet)
-router.register(r'groups', GroupViewSet)
+router.register('groups', GroupViewSet)
 
 urlpatterns = [
-    path('api/v1/api-token-auth/', views.obtain_auth_token),
-    path('api/v1/', include(router.urls))
+    path(f'api/{API_VERSION}/api-token-auth/', views.obtain_auth_token),
+    path(f'api/{API_VERSION}/', include(router.urls))
 ]
--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -7,13 +7,41 @@ from posts.models import Comment, Group, Post
 from .serializer import CommentSerializer, GroupSerializer, PostSerializer
 
 
+class AuthorPermissionMixin:
+    def handle_author_permission(self, instance, request):
+        response = check_author(instance, request)
+        if response:
+            return response
+
+    def update(self, request, *args, **kwargs):
+        partial = kwargs.pop('partial', False)
+        instance = self.get_object()
+        response = self.handle_author_permission(instance, request)
+        if response:
+            return response
+        serializer = self.get_serializer(
+            instance, data=request.data, partial=partial
+        )
+        serializer.is_valid(raise_exception=True)
+        self.perform_update(serializer)
+        return Response(serializer.data)
+
+    def destroy(self, request, *args, **kwargs):
+        instance = self.get_object()
+        response = self.handle_author_permission(instance, request)
+        if response:
+            return response
+        instance.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+
 def check_author(instance, request):
     if instance.author != request.user:
         return Response(status=status.HTTP_403_FORBIDDEN)
     return None
 
 
-class PostViewSet(viewsets.ModelViewSet):
+class PostViewSet(AuthorPermissionMixin, viewsets.ModelViewSet):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
     permission_classes = [permissions.IsAuthenticated]
@@ -21,71 +49,26 @@ class PostViewSet(viewsets.ModelViewSet):
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
 
-    def update(self, request, *args, **kwargs):
-        partial = kwargs.pop('partial', False)
-        instance = self.get_object()
-        response = check_author(instance, request)
-        if response:
-            return response
-        serializer = self.get_serializer(
-            instance, data=request.data, partial=partial
-        )
-        serializer.is_valid(raise_exception=True)
-        self.perform_update(serializer)
-        return Response(serializer.data)
 
-    def destroy(self, request, *args, **kwargs):
-        instance = self.get_object()
-        response = check_author(instance, request)
-        if response:
-            return response
-        instance.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
-
-
-class CommentViewSet(viewsets.ModelViewSet):
+class CommentViewSet(AuthorPermissionMixin, viewsets.ModelViewSet):
     queryset = Comment.objects.all()
     serializer_class = CommentSerializer
     permission_classes = [permissions.IsAuthenticated]
 
-    def get_queryset(self):
+    def get_post_object_or_404(self):
         post_id = self.kwargs.get('post_id')
-        post = get_object_or_404(Post, pk=post_id)
-        if post:
-            return Comment.objects.filter(post_id=post_id)
+        return get_object_or_404(Post, pk=post_id)
+
+    def get_queryset(self):
+        post_id = self.get_post_object_or_404()
+        return Comment.objects.select_related('post').filter(post_id=post_id)
 
     def perform_create(self, serializer):
-        post_id = self.kwargs.get('post_id')
-        post = get_object_or_404(Post, pk=post_id)
-        if post:
-            serializer.save(author=self.request.user, post_id=post_id)
-
-    def update(self, request, *args, **kwargs):
-        partial = kwargs.pop('partial', False)
-        instance = self.get_object()
-        response = check_author(instance, request)
-        if response:
-            return response
-        serializer = self.get_serializer(
-            instance, data=request.data, partial=partial
-        )
-        serializer.is_valid(raise_exception=True)
-        self.perform_update(serializer)
-        return Response(serializer.data)
-
-    def destroy(self, request, *args, **kwargs):
-        instance = self.get_object()
-        response = check_author(instance, request)
-        if response:
-            return response
-        instance.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+        post = self.get_post_object_or_404()
+        serializer.save(author=self.request.user, post=post)
 
 
-class GroupViewSet(viewsets.ModelViewSet):
+class GroupViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Group.objects.all()
     serializer_class = GroupSerializer
     permission_classes = [permissions.IsAuthenticated]
-
-    def create(self, request, *args, **kwargs):
-        return Response(status=status.HTTP_405_METHOD_NOT_ALLOWED)
